a06896eb96610db092a910d562221c79c4a4a11b
AdnanJami <118057773+AdnanJami@users.noreply.github.com>
2026-01-25 11:29:59 +0600

    Mask email credentials in settings.
    Replaced sensitive email credentials with placeholders.

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -174,9 +174,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'muntasiradnanj@gmail.com' 
-EMAIL_HOST_PASSWORD = 'rzha nibq gvzl pzei'  
-DEFAULT_FROM_EMAIL = 'muntasiradnanj@gmail.com'
+EMAIL_HOST_USER = '#######' 
+EMAIL_HOST_PASSWORD = '######'  
+DEFAULT_FROM_EMAIL = '######'
 
 
 # Site Configuration
